358567b4ace92992b85ff1b2ab4f7ab50ee0ef26
Ralph Ocdol <ralph.ocdol63@gmail.com>
2025-08-12 23:07:53 +0800

    (glimpse): follow color of logout button

--- glimpse/post-glance.js
+++ glimpse/post-glance.js
@@ -24,7 +24,7 @@
     const newElement = document.createElement('div');
     newElement.setAttribute('title', 'Launch Glimpse');
     newElement.innerHTML = `
-      <svg class="search-icon" stroke="var(--color-text-highlight)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+      <svg class="search-icon" stroke="var(--color-text-subdue)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"></path>
       </svg>
     `;
