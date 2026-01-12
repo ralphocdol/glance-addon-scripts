eddfbe3d934022b46482d66eeb1c5dc61352633d
ralphocdol <ralph.ocdol63@gmail.com>
2026-01-12 08:24:26 +0800

    (global-functions): initial

---
+++ global-functions/CREATE_ELEMENT.js
@@ -0,0 +1,46 @@
+window.CREATE_ELEMENT = function ({
+  tag = 'div',
+  id = null,
+  classes = null,
+  style = null,
+  props = null,
+  attrs = null,
+  datasets = null,
+  innerHTML = null,
+  innerText = null,
+  textContent = null,
+  events = null,
+} = {}) {
+  const e = document.createElement(tag);
+
+  if (id !== null) e.id = id;
+  if (style !== null) Object.assign(e.style, style);
+  if (props !== null) Object.assign(e, props);
+  if (classes !== null) e.className = classes;
+
+  if (innerHTML !== null) {
+    e.innerHTML = innerHTML;
+  } else if (innerText !== null) {
+    e.innerText = innerText;
+  } else if (textContent !== null) {
+    e.textContent = textContent;
+  }
+
+  if (datasets !== null) {
+    for (const [k, v] of Object.entries(datasets)) {
+      if (v == null) continue;
+      e.dataset[k] = String(v);
+    }
+  }
+
+  if (attrs !== null)
+    for (const [k, v] of Object.entries(attrs))
+      e.setAttribute(k, v);
+
+
+  if (events !== null)
+    for (const [event, handler] of Object.entries(events))
+      e.addEventListener(event, handler);
+
+  return e;
+}
---
+++ global-functions/README.md
@@ -0,0 +1,3 @@
+[⇐ Micro-script list](../#micro-scripts)
+
+This will be the place where redundant functions will be stored and will be a `window` function. Scripts here will be considered as `pre-DOM`.
