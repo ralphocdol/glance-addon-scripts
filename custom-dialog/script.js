8e8a686f79e65bfe8fefd494472c4077738229c6
Ralph Ocdol <ralph.ocdol63@gmail.com>
2026-01-22 08:43:36 +0800

    (custom-dialog): initial release (#34)
    * (custom-dialog): initial release
    
    * Update readmes

---
+++ custom-dialog/script.js
@@ -0,0 +1,93 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const createElementFn = window.CREATE_ELEMENT;
+  if (typeof createElementFn !== 'function') {
+    const msg = 'The global-function CREATE_ELEMENT not found, read the dependency in the README.md of this script.';
+    if (typeof window.showToast === 'function') window.showToast?.(msg, { title: 'CONFIRM_DIALOG', type: 'error' });
+
+    console.error('CREATE_ELEMENT not found');
+    window.customDialog = message => window.confirm(message);
+    return;
+  }
+
+  const dialog = createElementFn({
+    tag: 'dialog',
+    id: 'dialog',
+  });
+
+  document.body.appendChild(dialog);
+
+  async function customDialog(message, config) {
+    return new Promise(resolve => {
+      const {
+        type = 'confirm',
+        cancelText = 'CANCEL',
+        confirmText = 'CONFIRM',
+      } = config;
+
+      const container = createElementFn({
+        tag: 'form',
+        attrs: { method: 'dialog' },
+      });
+
+      const containerFrag = createElementFn({ isFragment: true });
+      switch (type) {
+        case 'confirm': {
+          container.setAttribute('dialog-confirm', '');
+          const messageEl = createElementFn({
+            tag: 'p',
+            textContent: message
+          });
+          containerFrag.appendChild(messageEl);
+
+          const dialogButtons = createElementFn({
+            classes: 'dialog-buttons',
+            htmlContent: `
+              <button value="no">${cancelText}</button>
+              <button value="yes" autofocus>${confirmText}</button>
+            `,
+          });
+
+          containerFrag.appendChild(dialogButtons);
+          break;
+        }
+        case 'info': {
+          container.setAttribute('dialog-info', '');
+          const messageEl = createElementFn({
+            tag: 'p',
+            textContent: message
+          });
+          containerFrag.appendChild(messageEl);
+
+          const dialogButtons = createElementFn({
+            classes: 'dialog-buttons',
+            htmlContent: `
+              <button value="no">${confirmText}</button>
+            `,
+          });
+
+          containerFrag.appendChild(dialogButtons);
+          break;
+        }
+      }
+      container.appendChild(containerFrag);
+
+      dialog.addEventListener('close', () => {
+        resolve(dialog.returnValue === 'yes');
+        setTimeout(() => {
+          dialog.style.display = 'none';
+          container.remove();
+          dialog.close();
+        }, 50);
+        dialog.classList.remove('show');
+      }, { once: true });
+
+      dialog.appendChild(container);
+      dialog.style.display = 'flex';
+      setTimeout(() => dialog.classList.add('show'), 50);
+      dialog.showModal();
+    });
+  }
+
+  window.customDialog = customDialog;
+});
