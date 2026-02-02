be1df367934e4a0586223a9fd7bb65a37b61663c
ralphocdol <ralph.ocdol63@gmail.com>
2026-02-02 08:24:40 +0800

    (custom-menu): dismissOnClick on the menu items

--- custom-menu/script.js
+++ custom-menu/script.js
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.error('CREATE_ELEMENT not found');
     return;
   }
+
   const headerNav = document.querySelector('.header-container > .header');
   const mobileNav = document.querySelector('.mobile-navigation > .mobile-navigation-icons');
   if (!headerNav || !mobileNav) return;
@@ -36,14 +37,15 @@ document.addEventListener('DOMContentLoaded', async () => {
   const mobileSearchNav = createCustomMenuElement(true);
   mobileSearchNav.classList.add('mobile-navigation-label');
 
-  const newCustomMenuItem = document.createElement('div');
-  newCustomMenuItem.classList.add('custom-menu-item');
-  newCustomMenuItem.innerHTML = `
-    <a href="#top" class="custom-menu-top">
-      <svg class="custom-menu-item-icon" viewBox="0 0 24 24" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 20L12 4M12 4L18 10M12 4L6 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-    </a>
-    <label>Top</label>
-  `;
+  const newCustomMenuItem = createElementFn({
+    classes: 'custom-menu-item',
+    htmlContent: `
+      <a href="#top" class="custom-menu-top">
+        <svg class="custom-menu-item-icon" viewBox="0 0 24 24" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 20L12 4M12 4L18 10M12 4L6 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+      </a>
+      <label>Top</label>
+    `,
+  });
 
   mobileSearchNav.querySelector('.custom-menu-items').appendChild(newCustomMenuItem);
   mobileNav.prepend(mobileSearchNav);
@@ -51,24 +53,37 @@ document.addEventListener('DOMContentLoaded', async () => {
   document.querySelector('a[href="#top"]:not(.custom-menu-top)').style.display = 'none';
 
   function createCustomMenuElement(mobile = false) {
-    let properties = `data-popover-position="below"`;
-    if (mobile) properties = `data-popover-position="above" data-popover-hide-delay="100" data-popover-anchor=".custom-menu-button" data-popover-trigger="click"`;
-    const newElement = document.createElement('div');
-    newElement.style.cursor = 'progress';
-    newElement.classList.add('custom-menu');
-    newElement.innerHTML = `
-      <div class="custom-menu-popover" data-popover-type="html" data-popover-show-delay="0" data-popover-max-width="500px" ${properties}>
-        <div data-popover-html>
-          <div class="custom-menu-items"></div>
-        </div>
-        ${mobile ? `<div class="pointer-events-none">` : ''}
-          <div class="custom-menu-button">
-            <svg fill="currentColor" version="1.1" viewBox="0 0 489 489" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M339.4,102c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V102z"></path> <path d="M488.9,102c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V102z"></path> <path d="M339.4,251.5c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V251.5z"></path> <path d="M488.9,251.5c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V251.5z"></path> <path d="M189.9,251.5c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V251.5z"></path> <path d="M321.3,382.9h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9c10,0,18.1-8.1,18.1-18.1V401 C339.4,391,331.3,382.9,321.3,382.9z"></path> <path d="M400.9,489h69.9c10,0,18.1-8.1,18.1-18.1V401c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9 C382.8,480.9,390.9,489,400.9,489z"></path> <path d="M171.8,382.9h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9c10,0,18.1-8.1,18.1-18.1V401 C189.9,391,181.8,382.9,171.8,382.9z"></path> <path d="M145,45.3v99.6H45.4V45.3H145 M162.2,0h-134C12.7,0,0.1,12.6,0.1,28.1v137.3c0,13.7,11.1,24.8,24.8,24.8h137.3 c15.5,0,28.1-12.6,28.1-28.1v-134C190.3,12.6,177.7,0,162.2,0L162.2,0z" fill="var(--color-primary)"></path> </g> </g> </g></svg>
-          </div>
-        ${mobile ? `</div>` : ''}
-      </div>
-    `;
-    return newElement;
+    return createElementFn({
+      classes: 'custom-menu',
+      styles: { cursor: 'progress' },
+      children: [
+        {
+          classes: 'custom-menu-popover',
+          datasets: {
+            popoverType: 'html',
+            popoverShowDelay: '0',
+            popoverHideDelay: '0',
+            popoverMaxWidth: '500px',
+            popoverPosition: mobile ? 'above' : 'below',
+            ...(mobile && { popoverPosition: 'above', popoverAnchor: '.custom-menu-button', popoverTrigger: 'click' })
+          },
+          children: [
+            { datasets: { popoverHtml: '' }, children: [ { classes: 'custom-menu-items' } ] },
+            {
+              classes: mobile ? 'pointer-events-none' : null,
+              children: [
+                {
+                  classes: 'custom-menu-button',
+                  htmlContent: `
+                    <svg fill="currentColor" version="1.1" viewBox="0 0 489 489" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M339.4,102c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V102z"></path> <path d="M488.9,102c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V102z"></path> <path d="M339.4,251.5c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V251.5z"></path> <path d="M488.9,251.5c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V251.5z"></path> <path d="M189.9,251.5c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9 c10,0,18.1-8.1,18.1-18.1V251.5z"></path> <path d="M321.3,382.9h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9c10,0,18.1-8.1,18.1-18.1V401 C339.4,391,331.3,382.9,321.3,382.9z"></path> <path d="M400.9,489h69.9c10,0,18.1-8.1,18.1-18.1V401c0-10-8.1-18.1-18.1-18.1h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9 C382.8,480.9,390.9,489,400.9,489z"></path> <path d="M171.8,382.9h-69.9c-10,0-18.1,8.1-18.1,18.1v69.9c0,10,8.1,18.1,18.1,18.1h69.9c10,0,18.1-8.1,18.1-18.1V401 C189.9,391,181.8,382.9,171.8,382.9z"></path> <path d="M145,45.3v99.6H45.4V45.3H145 M162.2,0h-134C12.7,0,0.1,12.6,0.1,28.1v137.3c0,13.7,11.1,24.8,24.8,24.8h137.3 c15.5,0,28.1-12.6,28.1-28.1v-134C190.3,12.6,177.7,0,162.2,0L162.2,0z" fill="var(--color-primary)"></path> </g> </g> </g></svg>
+                  `,
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    });
   }
 
   let menuList = [];
@@ -88,33 +103,30 @@ document.addEventListener('DOMContentLoaded', async () => {
   while (!document.body.classList.contains('page-columns-transitioned')) await new Promise(resolve => setTimeout(resolve, 50));
 
   setTimeout(() => {
-    let customMenuItems = document.querySelectorAll('.custom-menu-items');
+    const customMenuItems = document.querySelectorAll('.custom-menu-items');
     if (!customMenuItems) return;
+    const customMenuPopover = document.querySelector('.popover-container');
 
     menuList
       .sort((a, b) => (Number.isFinite(a.order) ? a.order : Infinity) - (Number.isFinite(b.order) ? b.order : Infinity) || a.className.localeCompare(b.className))
-      .forEach(({ className, tooltip, icon, actionFn, status, label }) => {
+      .forEach(({ className, tooltip, icon, actionFn, status, label, dismissOnClick = true }) => {
         const newStatus = typeof status === 'boolean' ? (status ? ' active' : ' inactive') : '';
 
         customMenuItems.forEach(menu => {
           const customItemEl = createElementFn({
             classes: 'custom-menu-item',
             props: { title: tooltip },
+            children: [
+              { classes: `${className}${newStatus}`, htmlContent: icon },
+              { tag: 'label', htmlContent: label || tooltip },
+            ],
+            events: {
+              click: e => {
+                if (dismissOnClick) customMenuPopover.dispatchEvent(new Event('mouseleave'));
+                actionFn(e);
+              },
+            }
           });
-
-          const navElement = createElementFn({
-            classes: `${className}${newStatus}`,
-            htmlContent: icon,
-          });
-          customItemEl.appendChild(navElement);
-
-          const navLabelElement = createElementFn({
-            tag: 'label',
-            htmlContent: label || tooltip,
-          });
-          customItemEl.appendChild(navLabelElement);
-
-          customItemEl.addEventListener('click', actionFn);
           menu.appendChild(customItemEl);
 
           const columns = Array.from(menu.querySelectorAll('.custom-menu-item')).length
