ec2887ba444264274cf2a6a14de662539e0e8a16
Ralph Ocdol <ralph.ocdol63@gmail.com>
2026-02-09 14:16:36 +0800

    (iframe-bookmark-nav): initial release

---
+++ iframe-bookmark-nav/script.js
@@ -0,0 +1,42 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', async () => {
+  // Catch duplicate instances
+  const scriptName = 'Iframe Bookmark Nav';
+  if ((window.GLANCE_ADDON_SCRIPTS ??= {})[scriptName] === true) {
+    const msg = scriptName + ' already loaded, you might have duplicate instance of this script. Aborting.';
+    if (typeof window.showToast === 'function') window.showToast?.(msg, { type: 'error' });
+    console.error(msg);
+    return;
+  } else {
+    window.GLANCE_ADDON_SCRIPTS[scriptName] = true;
+  }
+
+  while (!document.body.classList.contains('page-columns-transitioned')) await new Promise(resolve => setTimeout(resolve, 50));
+
+  const iframePage = { links: 'iframe-links-', page: 'iframe-page-' };
+  document.querySelectorAll(`[class^="${iframePage.links}"], [class*=" ${iframePage.links}"]`).forEach(el => {
+    const className = [...el.classList].find(c => c.startsWith(iframePage.links));
+    const classId = className.replace(iframePage.links, '');
+    const pageClass = `${iframePage.page}${classId}`;
+
+    const iframeContainer = document.querySelector(`.${pageClass}`);
+    if (!iframeContainer) throw new Error(`No iframe container found for class ${pageClass}`);
+    const iframeHeader = iframeContainer.querySelector('.widget-header h2');
+    const iframeHeaderLink = iframeHeader.querySelector('a');
+    const iframe = iframeContainer.querySelector('iframe');
+    if (!iframe) throw new Error(`No iframe found for class ${pageClass}`);
+
+    el.addEventListener('click', e => {
+      const link = e.target.closest('a');
+      if (!link) return;
+      iframe.contentWindow.location.href = link.href;
+      if (iframeHeaderLink) {
+        iframeHeaderLink.textContent = link.textContent;
+        iframeHeaderLink.href = link.href;
+      } else if (iframeHeader) {
+        iframeHeader.textContent = link.textContent;
+      }
+      e.preventDefault();
+    });
+  });
+});
